e8b9ed2c19f7ed47c86354928f91482bf137f52f
Sindre Moldeklev <sndrem@gmail.com>
2018-02-15 11:58:09 +0100

    Routes for quering tables and matches for a given tournament and season

---
+++ constants/constants.js
@@ -0,0 +1,6 @@
+const constants = {
+	TABLE_URL: 'http://api.tv2.no/sport/resources/table',
+	MATCHES_URL: 'http://api.tv2.no/sport/resources/matches'
+};
+
+export default constants;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+
+const axios = require('axios');
+
+const constants = require('../constants/constants.js').default;
+
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
 /**
@@ -13,7 +18,34 @@ router.get('/', (req, res) => {
 *
 * @apiParam {Number} tournamentId (e.g 230 for Premier League)
 * @apiParam {Number} seasonId (e.g 339 for Premier League in the season of 17/18)
-* 
 */
+router.get('/table/:tournamentId/:seasonId', (req, res) => {
+	const { tournamentId, seasonId } = req.params;
+	const url = `${constants.TABLE_URL}?start=0&max=38&tournamentId=${tournamentId}&seasonId=${seasonId}`;
+	axios.get(url)
+		.then((data) => {
+			res.json(data.data);
+		})
+		.catch((err) => {
+			res.json({
+				error: `Could not get table for url: ${url}`,
+				errorMessage: err
+			});
+		});
+});
+
+/**
+* @api {get} /matches/:tournamentId/:seasonId Get matches for a given tournament and season
+* @apiName GetMatches
+* @apiGroup Matches
+*
+* @apiParam {Number} tournamentId (e.g 230 for Premier League)
+* @apiParam {Number} seasonId (e.g 339 for Premier League in the season of 17/18)
+*/
+router.get('/matches/:tournamentId/:seasonId', (req, res) => {
+	const { tournamentId, seasonId } = req.params;
+	const url = `${constants.MATCHES_URL}?start=0&max=1000&tournamentId=${tournamentId}&seasonId=${seasonId}`;
+	axios.get(url).then(data => res.json(data.data)).catch(err => res.json({ error: `Could not get matches for ${url}`, errorMessage: err }));
+});
 
 module.exports = router;
