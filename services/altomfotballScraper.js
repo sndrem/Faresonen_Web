197a13f65aed5cb5594d321c867c4022f713eeb8
Sindre Moldeklev <sndrem@gmail.com>
2018-03-21 19:29:35 +0100

    Working on returning only players in the dangerzone

--- services/altomfotballScraper.js
+++ services/altomfotballScraper.js
@@ -4,127 +4,136 @@ const cheerio = require("cheerio");
 const constants = require("../constants/constants").default;
 
 function extractValues(tablerow, html) {
-	const place = parseInt(
-		html(tablerow)
-			.children("td")
-			.eq(0)
-			.text()
-			.replace(".", ""),
-		10
-	);
-	const name = html(tablerow)
-		.children("td")
-		.eq(1)
-		.find("a")
-		.eq(0)
-		.text();
-	const team = html(tablerow)
-		.children("td")
-		.eq(2)
-		.find("a")
-		.eq(0)
-		.text();
-	const value1 = parseInt(
-		html(tablerow)
-			.children("td")
-			.eq(3)
-			.text(),
-		10
-	);
-	const value2 = parseInt(
-		html(tablerow)
-			.children("td")
-			.eq(4)
-			.text(),
-		10
-	);
-	const value3 = parseFloat(
-		html(tablerow)
-			.children("td")
-			.eq(5)
-			.text()
-			.replace(",", ".")
-	);
+  const place = parseInt(
+    html(tablerow)
+      .children("td")
+      .eq(0)
+      .text()
+      .replace(".", ""),
+    10
+  );
+  const name = html(tablerow)
+    .children("td")
+    .eq(1)
+    .find("a")
+    .eq(0)
+    .text();
+  const team = html(tablerow)
+    .children("td")
+    .eq(2)
+    .find("a")
+    .eq(0)
+    .text();
+  const value1 = parseInt(
+    html(tablerow)
+      .children("td")
+      .eq(3)
+      .text(),
+    10
+  );
+  const value2 = parseInt(
+    html(tablerow)
+      .children("td")
+      .eq(4)
+      .text(),
+    10
+  );
+  const value3 = parseFloat(
+    html(tablerow)
+      .children("td")
+      .eq(5)
+      .text()
+      .replace(",", ".")
+  );
 
-	return {
-		place,
-		name,
-		team,
-		value1,
-		value2,
-		value3
-	};
+  return {
+    place,
+    name,
+    team,
+    value1,
+    value2,
+    value3
+  };
 }
 
 function getStatistics(url, tournamentId) {
-	return new Promise((resolve, reject) => {
-		if (isNaN(tournamentId)) {
-			reject(new Error("TournamentId is not valid. Must be a number"));
-		}
+  return new Promise((resolve, reject) => {
+    if (isNaN(tournamentId)) {
+      reject(new Error("TournamentId is not valid. Must be a number"));
+    }
 
-		request.get(`${url}${tournamentId}`, (err, response, html) => {
-			if (err) {
-				response.error = `Could not get statistics for url: ${url} with tournament ID: ${tournamentId}`;
-			}
-			const $ = cheerio.load(html);
-			const players = [];
-			$("table tbody tr").each((i, elem) => {
-				const playerData = extractValues(elem, $);
-				players.push(playerData);
-			});
-			resolve(players);
-		});
-	});
+    request.get(`${url}${tournamentId}`, (err, response, html) => {
+      if (err) {
+        response.error = `Could not get statistics for url: ${url} with tournament ID: ${tournamentId}`;
+      }
+      const $ = cheerio.load(html);
+      const players = [];
+      $("table tbody tr").each((i, elem) => {
+        const playerData = extractValues(elem, $);
+        players.push(playerData);
+      });
+      resolve(players);
+    });
+  });
+}
+
+function isInDangerzone(player) {
+  // TODO Remove !== 0 to === 0
+  return player.value1 > 0 && player.value1 % 2 !== 0;
+}
+
+function filterDangerzonePlayers(players) {
+  return players.filter(p => isInDangerzone(p));
 }
 
 const scraper = {
-	getYellowCards(req, res, next) {
-		getStatistics(constants.YELLOW_CARD_STATISTICS, req.params.tournamentId)
-			.then(data => {
-				res.players = data;
-				next();
-			})
-			.catch(err => {
-				throw new Error(err);
-			});
-	},
+  getYellowCards(req, res, next) {
+    getStatistics(constants.YELLOW_CARD_STATISTICS, req.params.tournamentId)
+      .then(data => {
+        res.players = data;
+        next();
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  },
 
-	getTopScorers(req, res, next) {
-		getStatistics(constants.TOPSCORER_STATISTICS, req.params.tournamentId)
-			.then(data => {
-				res.players = data;
-				next();
-			})
-			.catch(err => {
-				throw new Error(err);
-			});
-	},
+  getTopScorers(req, res, next) {
+    getStatistics(constants.TOPSCORER_STATISTICS, req.params.tournamentId)
+      .then(data => {
+        res.players = data;
+        next();
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  },
 
-	getAllDangerzonePlayers(req, res, next) {
-		const urls = [
-			getStatistics(constants.YELLOW_CARD_STATISTICS, 230),
-			getStatistics(constants.YELLOW_CARD_STATISTICS, 231)
-		];
+  getAllDangerzonePlayers(req, res, next) {
+    const urls = [
+      getStatistics(constants.YELLOW_CARD_STATISTICS, 1),
+      getStatistics(constants.YELLOW_CARD_STATISTICS, 2)
+    ];
 
-		axios
-			.all(urls)
-			.then(
-				axios.spread((pl, ch) => {
-					res.players = {
-						pl,
-						ch
-					};
-					next();
-				})
-			)
-			.catch(err => {
-				res.players = {
-					pl: [],
-					ch: []
-				};
-				next();
-			});
-	}
+    axios
+      .all(urls)
+      .then(
+        axios.spread((eliteserien, obosligaen) => {
+          res.players = {
+            eliteserien: filterDangerzonePlayers(eliteserien),
+            obosligaen: filterDangerzonePlayers(obosligaen)
+          };
+          next();
+        })
+      )
+      .catch(err => {
+        res.players = {
+          eliteserien: [],
+          obosligaen: []
+        };
+        next();
+      });
+  }
 };
 
 export default scraper;
