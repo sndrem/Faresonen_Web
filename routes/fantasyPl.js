bb35ddd1838999bfe9d28e0b4f34b17bad58e50d
Sindre Moldeklev <sndrem@gmail.com>
2018-04-09 14:35:27 +0200

    New build

--- routes/fantasyPl.js
+++ routes/fantasyPl.js
@@ -26,4 +26,29 @@ fantasyPlRouter.get("/teams", (req, res) => {
     .catch(err => res.status(400).json(err));
 });
 
+fantasyPlRouter.get("/player/:playerId", (req, res) => {
+  const { playerId } = req.params;
+
+  fplapi.getElements().then(data => {
+    const playerData = data.find(
+      player => player.id === parseInt(playerId, 10)
+    );
+    if (playerData) {
+      res.status(200).json(playerData);
+    } else res.status(400).json({});
+  });
+});
+
+fantasyPlRouter.get("/team/:teamId", (req, res) => {
+  const { teamId } = req.params;
+  fplapi.getTeams().then(data => {
+    const team = data.find(t => {
+      return t.code === parseInt(teamId, 10);
+    });
+    if (!team) {
+      res.status(400).json({});
+    } else res.status(200).json(team);
+  });
+});
+
 module.exports = fantasyPlRouter;
