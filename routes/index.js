ff50fe7a98ffb669466335e661438e08adbf0b8e
Sindre Moldeklev <sndrem@gmail.com>
2018-05-28 09:04:10 +0200

    Testing some scripts

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
-console.log("Index.js");
 const express = require("express");
 
-console.log("Required express");
 const axios = require("axios");
 
 const constants = require("../constants/constants.js").default;
