5212279800b2258c89423d903114656f0fd6d5ea
Sindre Moldeklev <sndrem@gmail.com>
2019-03-09 08:56:14 +0100

    New deploy

---
+++ public/precache-manifest.d1a9c669705aeec53d6632a91a094627.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "0bad9befdbfd3f35ef2b",
+    "url": "/static/js/main.0192e60a.chunk.js"
+  },
+  {
+    "revision": "b6a3e824e20a95bb2f0b",
+    "url": "/static/js/2.00c182ea.chunk.js"
+  },
+  {
+    "revision": "0bad9befdbfd3f35ef2b",
+    "url": "/static/css/main.e53b32e3.chunk.css"
+  },
+  {
+    "revision": "c681bd7eb8395d768c3efee9429eceea",
+    "url": "/index.html"
+  }
+];
